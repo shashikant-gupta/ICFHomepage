a39b88d2623bca0dfc388df239a48ea8554c25ec
shashikant-gupta <33100079+shashikant-gupta@users.noreply.github.com>
2017-11-16 19:36:44 +0530

    DealPart

--- assets/js/vendor/Homepage.js
+++ assets/js/vendor/Homepage.js
@@ -1,4 +1,4 @@
-$(function() {
+/*$(function() {
         'use strict';
         $(document).ready(function(){
             $(".filter-deals").click(function(){
@@ -23,9 +23,40 @@ $(function() {
 
 
             });
+            document.querySelector( "#nav-toggle" )
+  .addEventListener( "click", function() {
+    this.classList.toggle( "active" );
+  });
 
+        });        
+
+    });*/
+$(document).ready(function() { 
+
+    (function ($) { 
+        
+        $('ul.dealTabs li ').click(function (g) {
+            $('html').animate({
+                        scrollTop: $(".dealTabs").offset().top
+               }, 1500);
+            var tab = $(this).closest('.tab'), 
+                index = $(this).closest('li').index();                
             
+            tab.find('ul.dealTabs > li').removeClass('active');
+            $(this).closest('li').addClass('active');
+            
+            tab.find('.icfstatic_deals').find('div.tabs_item').not('div.tabs_item:eq(' + index + ')').slideUp();
+            tab.find('.icfstatic_deals').find('div.tabs_item:eq(' + index + ')').slideDown();
+            
+            g.preventDefault();
+        } );
+    })(jQuery);
 
-        });
 
-    });
+   document.querySelector( "#nav-toggle" )
+  .addEventListener( "click", function() {
+    this.classList.toggle( "active" );
+  });
+
+
+});
