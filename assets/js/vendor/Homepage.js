762b355c45cf30c73a6d8cfbcac5f7ac8a9c0db9
shashikant-gupta <33100079+shashikant-gupta@users.noreply.github.com>
2017-12-06 16:33:58 +0530

    header

--- assets/js/vendor/Homepage.js
+++ assets/js/vendor/Homepage.js
@@ -1,36 +1,3 @@
-/*$(function() {
-        'use strict';
-        $(document).ready(function(){
-            $(".filter-deals").click(function(){
-                var value = $(this).attr('data-filter');
-                if(value == "all")
-                {
-                    //$('.filter').removeClass('hidden');
-                    $('.items-filter').show('1000');
-                }
-                else
-                {
-//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
-//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
-                    $(".items-filter").not('.'+value).hide('3000');
-                    $('.items-filter').filter('.'+value).show('3000');
-                    if ($(".filter-deals").removeClass("active")) {
-                $(this).removeClass("active");
-            }
-            $(this).addClass("active");
-
-                }
-
-
-            });
-            document.querySelector( "#nav-toggle" )
-  .addEventListener( "click", function() {
-    this.classList.toggle( "active" );
-  });
-
-        });        
-
-    });*/
 $(document).ready(function() { 
 
     (function ($) { 
@@ -60,3 +27,12 @@ $(document).ready(function() {
 
 
 });
+
+////////////////////////////////////////////// Header scroll ///////////////////////////////////////////////////////////////////////////////////
+$(window).scroll(function(){
+    var sticky=$("#header");
+    scroll=$(window).scrollTop();
+    
+    if(scroll>=100) sticky.addClass("fixed");
+    else sticky.removeClass("fixed");
+    });
